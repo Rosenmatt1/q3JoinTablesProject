48d0fe412974cc0d79224c6cf4e8b357d1ef3cd1
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-15 10:39:00 -0700

    Created developer enviorment and vairables

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 const express = require('express')
+const app = express()
+const port = process.env.PORT || 3005
+
+const env = process.env.NODE_ENV || 'development'
+const config = require('./knexfile.js')[env]
+const knex = require('knex')(config)
