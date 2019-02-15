acdf8466aa29cbb9a9cfddf57ea1f63735d3aea5
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-15 11:33:27 -0700

    Filled in migration table roommates toinclude foreign key

--- migrations/20190215111353_roommates.js
+++ migrations/20190215111353_roommates.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('roommates', table => {
+    table.increments()
+    table.string('name').notNullable().defaultsTo('Person')
+    table.integer('age').notNullable().defaultsTo(18)
+    table.integer('household_id')
+    table.foreign('household_id').references('id').inTable('households')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('roommates')
 };
