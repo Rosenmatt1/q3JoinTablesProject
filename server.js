1404483880d6782cc7c54219a411433ae97ad596
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-15 16:34:22 -0700

    Successfully completed get route that selects just the roomate name and chore

--- server.js
+++ server.js
@@ -13,8 +13,18 @@ const dotenv = require("dotenv")
 app.use(parser.json())
 app.use(cors())
 
-// app.get('/', (req, res) => {
-//   knex
-// })
+app.get('/', (req, res) => {
+  return knex('households')
+    .then(households => {
+      const getHouseDuty = households.map(household => {
+        return knex('chores')
+          .join('roommates', 'roommates.id', 'chores.roommate_id')
+          .where('roommates.id', household.id )
+          .select('roommates.name', 'chores.chore')
+        return household
+      })
+      return Promise.all(getHouseDuty).then(result => res.send(result))
+    })
+})
 
 app.listen(port, () => console.log(`Cleaning up the house on port ${port}!`))
