5e955cd54dfa8705b01098a154767bc95e09c9e5
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-15 11:22:24 -0700

    added the up and down knex code to create and drop households tale

--- migrations/20190215111105_households.js
+++ migrations/20190215111105_households.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('households', table => {
+    table.increments()
+    table.string('address').notNullable().defaultsTo('')
+    table.string('name')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('households')
 };
