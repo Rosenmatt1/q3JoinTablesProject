4c14ca09ce754acb310afd20d2f09298e71503b1
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-21 14:56:48 -0500

    Refactored so that the .then roommates is chores to appropriatly relfect the data that was returned.  Use chore instead of roommate in the .map as well

--- server.js
+++ server.js
@@ -15,17 +15,33 @@ app.use(cors())
 
 app.get('/', (req, res) => {
   return knex('chores')
-    .then(roommates => {
-      const getHouseDuty = roommates.map(roommate => {
+    .then(chores => {
+      const getHouseDuty = chores.map(chore => {
         return knex('roommates')
-          // .join('households', 'households.id', 'roommates.household_id')
+          .join('households', 'households.id', 'roommates.household_id')
           .join('chores', 'roommates.id', 'chores.roommate_id')
-          .where('roommates.id', roommate.id)
+          .where('roommates.id', chore.id)
           // .andWhere('households.id', 'roommates.household_id')
           .select('roommates.name', 'chores.chore')
       })
       return Promise.all(getHouseDuty).then(result => res.send(result))
     })
 })
+
+// app.get('/', (req, res) => {
+//   return knex('roommates')
+//     .then(roommates => {
+//       const getHouseDuty = roommates.map(roommate => {
+//         return knex('chores')
+//           .join('households', 'households.id', 'roommates.household_id')
+//           .join('chores', 'roommates.id', 'chores.roommate_id')
+//           .where('roommates.id', roommate.id)
+//           .where(roommate.id, 'chores.roommate_id')
+//           .where('households.id', roommate.household_id)
+//           .select('roommates.name', 'chores.chore')
+//       })
+//       return Promise.all(getHouseDuty).then(result => res.send(result))
+//     })
+// })
 
 app.listen(port, () => console.log(`Cleaning up the house on port ${port}!`))
