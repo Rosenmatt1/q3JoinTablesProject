77123fe28293ba59a62308f853930705e534e7b9
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-22 18:26:33 -0500

    Created new get route based on households

--- server.js
+++ server.js
@@ -26,36 +26,22 @@ app.get('/', (req, res) => {
     })
 })
 
-// .join('households', 'households.id', 'roommates.household_id')
-// .where('households.id', 'roommates.household_id')
-
-// app.get('/:id', (req, res) => {
-//   return knex('roommates')
-//     .where('household_id', req.params.id)
-//       .then(roommates => {
-//           const roommateChores = roommates.map(roommate => {
-//             knex('chores')
-//               .where('roommate_id', roommate.id)
-//               .then(roommateChore => console.log("house2", roommateChore))
-//               return roommate
-//           })
-//       })
-// })
-
-// app.get('/', (req, res) => {
-//   return knex('roommates')
-//     .then(roommates => {
-//       const getHouseDuty = roommates.map(roommate => {
-//         return knex('chores')
-//           .join('households', 'households.id', 'roommates.household_id')
-//           .join('chores', 'roommates.id', 'chores.roommate_id')
-//           .where('roommates.id', roommate.id)
-//           .where(roommate.id, 'chores.roommate_id')
-//           .where('households.id', roommate.household_id)
-//           .select('roommates.name', 'chores.chore')
-//       })
-//       return Promise.all(getHouseDuty).then(result => res.send(result))
-//     })
-// })
+app.get('/:id', (req, res) => {
+  return knex('roommates')
+    .where('roommates.household_id', req.params.id)
+      .then(roommates => {
+          const roommateChores = roommates.map(roommate => {
+            return knex('chores')
+              .where('chores.roommate_id', roommate.id)
+              .select('chore')
+              .then(roommateChore => {
+                roommate.chores = roommateChore 
+                return roommate
+              })
+          })
+        console.log(roommateChores)
+        return Promise.all(roommateChores).then(result => res.send(result))
+      })
+})
 
 app.listen(port, () => console.log(`Cleaning up the house on port ${port}!`))
