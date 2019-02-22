d492df65c9d3929a365ab579b9e1b8f7bc66ac93
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-22 16:19:09 -0500

    Refactoring so that first get route just returns all roommates that have a chore

--- server.js
+++ server.js
@@ -18,15 +18,29 @@ app.get('/', (req, res) => {
     .then(chores => {
       const getHouseDuty = chores.map(chore => {
         return knex('roommates')
-          .join('households', 'households.id', 'roommates.household_id')
           .join('chores', 'roommates.id', 'chores.roommate_id')
-          .where('roommates.id', chore.id)
-          // .andWhere('households.id', 'roommates.household_id')
+          .where('roommates.id', chore.roommate_id)
           .select('roommates.name', 'chores.chore')
       })
       return Promise.all(getHouseDuty).then(result => res.send(result))
     })
 })
+
+// .join('households', 'households.id', 'roommates.household_id')
+// .where('households.id', 'roommates.household_id')
+
+// app.get('/:id', (req, res) => {
+//   return knex('roommates')
+//     .where('household_id', req.params.id)
+//       .then(roommates => {
+//           const roommateChores = roommates.map(roommate => {
+//             knex('chores')
+//               .where('roommate_id', roommate.id)
+//               .then(roommateChore => console.log("house2", roommateChore))
+//               return roommate
+//           })
+//       })
+// })
 
 // app.get('/', (req, res) => {
 //   return knex('roommates')
